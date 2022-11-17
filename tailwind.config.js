5dd9239c8056f36451a4e748dd454083d913f6e4
Vherniel Lebis <hello@vherniellebis.tech>
2022-11-17 19:47:48 +0800

    BREAKING CHANGE: additional breakpoints and use "em" unit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,18 @@
 module.exports = {
     content: [
         "./app/**/*.{js,ts,jsx,tsx}",
-        "./src/**/*.{js,ts,jsx,tsx}"
+        "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
+        screens: {
+            ["xxs"]: "20em",    // @media (min-width: 320px)
+            xs: "30em",         // @media (min-width: 480px)
+            sm: "40em",         // @media (min-width: 640px)
+            md: "48em",         // @media (min-width: 768px)
+            lg: "64em",         // @media (min-width: 1024px)
+            xl: "80em",         // @media (min-width: 1280px)
+            ["xxl"]: "96em",    // @media (min-width: 1536px)
+        },
         extend: {},
     },
     plugins: [],
