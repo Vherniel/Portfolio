4a7855da4608cd1a788517f97603a87db984c3f8
Vherniel Lebis <hello@vherniellebis.tech>
2022-11-17 19:50:12 +0800

    feat: implement @next/font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,31 @@
+import { Manrope } from "@next/font/google";
+import clsx from "clsx";
 import "./globals.scss";
+
+const font = Manrope({
+    fallback: [
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Segoe UI",
+        "Roboto",
+        "Oxygen",
+        "Ubuntu",
+        "Cantarell",
+        "Fira Sans",
+        "Droid Sans",
+        "Helvetica Neue",
+        "sans-serif",
+    ],
+    subsets: ["latin"],
+    variable: "--manrope",
+    weight: "variable",
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
             <head />
-            <body>{children}</body>
+            <body className={clsx(font.variable)}>{children}</body>
         </html>
     );
 }
